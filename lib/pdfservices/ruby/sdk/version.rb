85406278c102550d2489ab34b8398f56dd1b6934
Jimmy Bosse <jimmy.bosse@ankura.com>
2022-08-25 09:52:38 -0400

    Initial commit.

---
+++ lib/pdfservices/ruby/sdk/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Pdfservices
+  module Ruby
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "pdfservices/ruby/sdk"
+
+require "minitest/autorun"
