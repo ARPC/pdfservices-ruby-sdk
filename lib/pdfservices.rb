1dc1664b3e8eb675d231cd28b64025d294d006f9
Jimmy Bosse <jimmy.bosse@ankura.com>
2022-08-31 13:14:29 -0400

    Do it like devise

--- lib/pdfservices.rb
+++ lib/pdfservices.rb
@@ -4,13 +4,15 @@ require "http"
 require "json"
 require "jwt"
 require "multipart_parser/reader"
-require "pdfservices/document_merge/operation"
-require "pdfservices/document_merge/result"
-require "pdfservices/credentials_builder"
-require "pdfservices/credentials"
-require "pdfservices/jwt_provider"
-require "pdfservices/version"
 require "yaml"
 
 module PdfServices
+  autoload :CredentialsBuilder, "pdfservices/credentials_builder"
+  autoload :Credentials, "pdfservices/credentials"
+  autoload :JwtProvider, "pdfservices/jwt_provider"
+
+  module DocumentMerge
+    autoload :Operation, "pdfservices/document_merge/operation"
+    autoload :Result, "pdfservices/document_merge/result"
+  end
 end
